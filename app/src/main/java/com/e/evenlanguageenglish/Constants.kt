8b4166a61e7a549847cd8275059c23806ede414f
t0in4 <t0in4@yahoo.com>
2021-01-17 15:31:57 +0300

    Initial commit

---
+++ app/src/main/java/com/e/evenlanguageenglish/Constants.kt
@@ -0,0 +1,149 @@
+package com.e.evenlanguageenglish
+
+import java.util.*
+import kotlin.collections.ArrayList
+
+object Constants {
+    fun getQuestions(): ArrayList<Questions> {
+        val questionsList = ArrayList<Questions> ()
+
+        val que1 = Questions(1, "What meaning does this word 'Anmanrakan' belong to?",
+        R.drawable.a, "Elecampane", "Nonagenarian", "Quoad",
+            "Rill", 4
+        )
+        questionsList.add(que1)
+        val que2 = Questions(2, "What meaning does this word 'Boripca' belong to?",
+            R.drawable.b1, "Quotient", "Seine", "Perennial",
+            "Improve", 1
+        )
+        questionsList.add(que2)
+        val que3 = Questions(3, "What meaning does this word 'Chagarap' belong to?",
+            R.drawable.c1, "Tinder", "Year before last", "Slender",
+            "Omissible", 2
+        )
+        questionsList.add(que3)
+        val que4 = Questions(4, "What meaning does this word 'Dokal' belong to?",
+                R.drawable.d1, "Butterfly", "Lichen", "Palaver",
+                "Surra", 1
+        )
+        questionsList.add(que4)
+        val que5 = Questions(5, "What meaning does this word 'ʒapkan' belong to?",
+                R.drawable.japkan1, "Chela", "Spirit", "Pure",
+                "Eight", 4
+        )
+        questionsList.add(que5)
+        val que6 = Questions(6, "What meaning does this word 'Elan' belong to?",
+                R.drawable.e1, "Chert", "Three", "Destrier",
+                "Gannet", 2
+        )
+        questionsList.add(que6)
+        val que7 = Questions(7, "What meaning does this word 'Әdәn' belong to?",
+                R.drawable.ae, "Toiler", "Scut", "Wind, air",
+                "Ogee", 3
+        )
+        questionsList.add(que7)
+        val que8 = Questions(8, "What meaning does this word 'ɝripci' belong to?",
+                R.drawable.ethiopic, "Alluvion", "Prognosis", "Cheap",
+                "Expensive", 4
+        )
+        questionsList.add(que8)
+        val que9 = Questions(9, "What meaning does this word 'Fizika' belong to?",
+                R.drawable.f, "Conation", "Physics", "Febrile",
+                "Induce", 2
+        )
+        questionsList.add(que9)
+        val que10 = Questions(10, "What meaning does this word 'Gegan' belong to?",
+                R.drawable.g, "Mazarine", "Jerboa", "Grindstone",
+                "Bronze, yellow copper", 4
+        )
+        questionsList.add(que10)
+        val que11 = Questions(11, "What meaning does this word 'Hana' belong to?",
+                R.drawable.h, "Trouble", "Hoax", "Lull",
+                "Mist", 1
+        )
+        questionsList.add(que11)
+        val que12 = Questions(12, "What meaning does this word 'Ilin' belong to?",
+                R.drawable.i, "Morning star, inner side", "Knout",
+                "Quartan",
+                "Evening star, outer side", 4
+        )
+        questionsList.add(que12)
+        val que13 = Questions(13, "What meaning does this word 'Juit' belong to?",
+                R.drawable.j, "Trouble", "Eskimo", "Lull",
+                "Mist", 2
+        )
+        questionsList.add(que13)
+        val que14 = Questions(14, "What meaning does this word 'Kacikan' belong to?",
+                R.drawable.k, "Racoon", "Poach", "Sheen",
+                "Pup", 4
+        )
+        questionsList.add(que14)
+        val que15 = Questions(15, "What meaning does this word 'Luorawetlan' belong to?",
+                R.drawable.k, "Surplusage", "Thyme", "Chukchee",
+                "Winnow", 3
+        )
+        questionsList.add(que15)
+        val que16 = Questions(16, "What meaning does this word 'Magʒili' belong to?",
+                R.drawable.m, "Woody", "Worm", "Utter",
+                "Rodent", 2
+        )
+        questionsList.add(que16)
+        val que17 = Questions(17, "What meaning does this word 'Nabgildiwun' belong to?",
+                R.drawable.n, "Lapwing", "Drizzle", "Carcajou",
+                "Glue", 4
+        )
+        questionsList.add(que17)
+        val que18 = Questions(18, "What meaning does this word 'Ņaya' belong to?",
+                R.drawable.ng1, "Shaman's song", "Carboy", "Eland",
+                "Hardpan", 4
+        )
+        questionsList.add(que18)
+        val que19 = Questions(19, "What meaning does this word 'Ŋagran' belong to?",
+                R.drawable.ng, "Rencontre", "Outlander", "Spatial",
+                "May Queen", 2
+        )
+        questionsList.add(que19)
+        val que20 = Questions(20, "What meaning does this word 'Okun' belong to?",
+                R.drawable.ng, "Myrrh", "Insobriety", "Rust",
+                "Rusticate", 3
+        )
+        questionsList.add(que20)
+        val que21 = Questions(21, "What meaning does this word 'Pujpanran' belong to?",
+                R.drawable.p, "Chirm", "Pale", "Instance",
+                "Swarming", 4
+        )
+        questionsList.add(que21)
+        val que22 = Questions(22, "What meaning does this word 'Radiomagәn' belong to?",
+                R.drawable.r, "Radioamateur", "Solitude", "Radioman",
+                "Topi", 1
+        )
+        questionsList.add(que22)
+        val que23 = Questions(23, "What meaning does this word 'Scetic hawanmaj, hadalginmaj' belong to?",
+                R.drawable.s, "Addition and subtraction in mind", "Addition and subtraction on computer", "Addition and subraction on accounts",
+                "Addition and subtraction on abacus", 4
+        )
+        questionsList.add(que23)
+        val que24 = Questions(24, "What meaning does this word 'Tabdas' belong to?",
+                R.drawable.t, "Watchword", "Take", "Through",
+                "Just", 3
+        )
+        questionsList.add(que24)
+        val que25 = Questions(25, "What meaning does this word 'Umtacan' belong to?",
+                R.drawable.u, "Crowberry", "Strawberry", "Lingonberry",
+                "Cranberry", 1
+        )
+        questionsList.add(que25)
+        val que26 = Questions(26, "What meaning does this word 'Wak-wak' belong to?",
+                R.drawable.eu, "Grunt", "Squeak", "Croak",
+                "Hoot", 3
+        )
+        questionsList.add(que26)
+        val que27 = Questions(27, "What meaning does this word 'Zolotomrin oņiŋ' belong to?",
+                R.drawable.z, "Alluvial gravel", "Auriferous gravel", "River gravel",
+                "Bank gravel", 2
+        )
+        questionsList.add(que27)
+        Collections.shuffle(questionsList)
+        return questionsList
+    }
+}
